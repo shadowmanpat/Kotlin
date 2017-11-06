95955a3f6998575da760ffbab19f3a94094028d2
Nikolaos Agas <nagas@vanderbox.gr>
2017-11-06 15:29:48 +0200

    Initial commit

---
+++ Learn Kotlin —  apply vs with_files/google-analytics.js
@@ -0,0 +1,18 @@
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', 'UA-91686131-1', 'auto');
+
+// Replace the following lines with the plugins you want to use.
+ga('require', 'cleanUrlTracker');
+ga('require', 'eventTracker');
+ga('require', 'impressionTracker');
+ga('require', 'outboundLinkTracker');
+ga('require', 'urlChangeTracker');
+ga('require', 'maxScrollTracker');
+ga('require', 'mediaQueryTracker');
+ga('require', 'outboundFormTracker');
+ga('require', 'outboundLinkTracker');
+ga('require', 'pageVisibilityTracker');
+ga('require', 'socialWidgetTracker');
+ga('require', 'urlChangeTracker');
+
+ga('send', 'pageview');
